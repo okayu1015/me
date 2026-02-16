5bdc716d3d8a5256a8383830039c48c8ae72d2f3
okayu1015 <33214171+okayu1015@users.noreply.github.com>
2026-02-16 07:43:55 +0000

    Deploying to gh-pages from @ okayu1015/me@55c73f8dc73d08805ba7591fb7e3b07360d2d4fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about-ja",
-    title: "about(ja)",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/me/";
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/me/";
+            window.location.href = "/me/_pages/about_ja/";
           },
         },{id: "nav-publications",
           title: "publications",
