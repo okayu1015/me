faa6f4c7f0c10baf1ce49894b699382b2b18f7bb
okayu1015 <33214171+okayu1015@users.noreply.github.com>
2026-02-19 06:55:37 +0000

    Deploying to gh-pages from @ okayu1015/me@43f0ff90beaf7afd64ddb7808abed4847017bcc5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/me/ja/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/me/teaching/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
